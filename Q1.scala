8fd3062c5c3d1bc25b73dac501aa2df40d5d3595
AchalaHM <92708168+AchalaHM@users.noreply.github.com>
2022-09-06 08:01:13 +0530

    Add files via upload

---
+++ Q1.scala
@@ -0,0 +1,30 @@
+object Q1 extends App {
+  val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+  val message = scala.io.StdIn.readLine("Enter the secret message: ")
+  val shift = (scala.io.StdIn.readLine("Shift number: ").toInt + alphabet.length) % alphabet.length
+
+// Encription
+  val Encryption = (shift:Int)=>message.map( (c: Char) => {
+    val x = alphabet.indexOf(c.toUpper)
+    if (x == -1) c
+    else alphabet((x + shift + alphabet.length) % alphabet.length)
+  })
+
+//Decription
+  val Decryption = (shift:Int)=>message.map( (c: Char) => {
+
+    val x = alphabet.indexOf(c.toUpper)
+    if (x == -1) c
+    else alphabet((x - shift + alphabet.length) % alphabet.length)
+  })
+
+  println("Select operation(1 / 2)")
+  println("1.Encryption\n2.Decription")
+
+  val choice=scala.io.StdIn.readInt()
+
+  if(choice==1) println("Encrypted message: "+ Encryption(shift))
+
+  else println("Decrypted message: "+ Decryption(shift))
+}
